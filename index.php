7ca38e62d32926147cd351f2df65bc4b2d890c1c
LarisaKondratenko <122371979+LarisaKondratenko@users.noreply.github.com>
2023-05-14 20:32:04 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?
+
+include "main.php";
+
+
+// getPerfectPartner("Славин", "Семён", "Сергеевич", $example_persons_array);
+getGenderDescription($example_persons_array);
+  
+
+?>
---
+++ main.php
@@ -0,0 +1,4 @@
+<?  
+
+include "person.php"; 
+include "functions_massiw.php";
